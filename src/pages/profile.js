0014879bdc9b071ac82c6b0b303e2b494e513916
Kenny Mark <geniounico@outlook.com>
2020-01-12 12:30:08 +0000

    fixed typo;

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -10,7 +10,7 @@ export default function Profiles() {
       <div className="flex-col lg:flex p-3 justify-between sm:w-5/6 mx-auto tracking-wide" data-sal='fade' data-sal-duration='400'>
         <h1 className='text-4xl text-center'>About Me</h1>
         <p className='text-xl text-gray-600 leading-snug'>
-          I’m Kenny Mark, currently a Software Engineer for SABS Travel Technologies. I am front-end focused with full stack experience. With a total of 5 years of experience, 3 years freelancing,studying and building hobby projects and 2 years commercial I have worked on every kind of application be it small, medium or very large enterprise application. I love everything UI and UX on the web but I have also worked on developed mobile applications, web apps and some backend applications.
+          I’m Kenny Mark, currently a Software Engineer for SABS Travel Technologies. I am front-end focused developer with full stack experience. With a total of 5 years of experience, 3 years freelancing,studying and building hobby projects and 2 years commercial I have worked on every kind of application be it small, medium or very large enterprise application. I love everything UI and UX on the web but I have also worked on developed mobile applications, web apps and some backend applications.
           </p>
         <p className='text-xl mt-2 text-gray-600 leading-snug'>
           I actively spend many hours per day working on my side projects which can be found on 
