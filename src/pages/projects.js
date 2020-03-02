a8b16f7c797c8eb9f2093633a197b2f33238a263
Kenny Mark <geniounico@outlook.com>
2020-03-02 08:53:41 +0000

    projects page looks much nicer

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -21,24 +21,25 @@ export default function Projects() {
   return (
     <Layout>
       <SEO title="Projects" />
-      <h1 className='text-gray-800 lg:text-left sm:text-center mb-2 text-2xl sm:text-center '>Top Projects</h1>
+      <h1 className='text-gray-800 lg:text-left sm:text-center mb-2 text-2xl sm:text-center ml-1'>Top Projects</h1>
       <Masonry
-        breakpointCols={{ default: 2, 800: 1 }}
+        breakpointCols={{ default: 3, 800: 1 }}
         className="my-masonry-grid relative"
         columnClassName="my-masonry-grid_column">
         {topProjects.reverse().map((project, idx) => (
-          <div style={{ background: project.color }} className='rounded-lg p-5 shadow m-2 cursor-pointer relative'>
-            <h2 className='capitalize text-center text-lg mb-2 text-gray-900'>{project.name} </h2>
+          <div style={{ background: project.color }} className='rounded-lg p-5 shadow m-2 cursor-pointer relative proj'>
+            <h2 className='capitalize text-center text-normal mb-3 text-black font-semibold text-gray-700 '>{project.name} </h2>
 
-            <img src={project.image} alt='' className='rounded' />
+            <img src={project.image} alt='' className='rounded-md shadow-lg' />
 
-            <div className="bg-gray-700 w-full  h-full absolute flex flex-col justify-center items-center mt-4 p-4"
+            <div style={{ backgroundImage: project.color, backgroundColor: 'rgba(0,0,0,2)', backgroundBlendMode: 'color' }}
+              className=" bg-gray-700 w-full h-full absolute flex flex-col justify-center items-center p-4 desc"
               style={{ bottom: 0, borderRadius: 'inherit', right: 0 }}>
 
-              <p className="text-gray-100 text-md mb-5 ">{project.description}</p>
+              <p className="text-gray-100 text-md mb-5 w-64 ">{project.description}</p>
 
-              <button>
-                <a href={project.link} target='_blank' rel="noopener noreferrer" className=' px-6 py-2 rounded text-gray-600 bg-white mr-2  hover:bg-black hover:text-gray-100'> View</a>
+              <button className='mt-5'>
+                <a href={project.link} target='_blank' rel="noopener noreferrer" className='px-6 py-2 rounded text-gray-600 bg-white mr-2  hover:bg-black hover:text-gray-100'> View</a>
 
                 <a href={project.github} target='_blank' rel="noopener noreferrer" className='px-6 py-2 rounded text-gray-600 bg-white hover:bg-black hover:text-gray-100'> Github</a>
               </button>
@@ -83,9 +84,9 @@ export default function Projects() {
           <div className="bg-gray-800 rounded p-3 shadow-md my-4 mx-3" key={idx} data-sal="slide-down" data-sal-delay="200"
             data-sal-easing="ease" >
             <div className="flex justify-between ">
-              <h2 className='capitalize text-gray-300'>{project.name} </h2>
+              <h2 className='capitalize text-gray-200'>{project.name} </h2>
 
-              <a href={project.link} target='_blank' rel="noopener noreferrer" className=' px-6 py-1 rounded text-sm text-gray-100 bg-black ml-2 focus hover:bg-white hover:text-black'> View</a>
+              <a href={project.link} target='_blank' rel="noopener noreferrer" className='px-6 py-1 rounded text-sm text-white bg-gray-900 ml-2 focus hover:bg-white hover:text-black'> View</a>
 
             </div>
             <p className='text-gray-500 w-4/5 normal-case'>{project.description}</p>
--- src/pages/skills.js
+++ src/pages/skills.js
@@ -11,16 +11,8 @@ import frontend from './../images/front-end.svg';
 
 export default function Skills() {
 
-  const skillClasses = 'flex-1 bg-gray-900 mt-4 border rounded-lg sm:w-full shadow-md m-3 px-6 pb-3'
-  // const svgImg = {
-  //   zIndex: 10,
-  //   float: 'right',
-  //   maxWidth: 350,
-  //   height: 350,
-  //   opacity: 0.2,
-  //   position: 'absolute',
-  //   margin: '0 auto'
-  // }
+  const skillClasses = 'flex-1 bg-gray-800 mt-4 border rounded-lg sm:w-full shadow-md m-3 px-6 pb-3'
+
   const svgbg = {
     backgroundPosition: 'center',
     backgroundBlendMode: 'color-burn',
@@ -34,7 +26,7 @@ export default function Skills() {
 
       <div className="flex-row lg:flex" data-sal="slide-down" data-sal-delay="200">
         <div className={skillClasses} style={{ backgroundImage: `url(${frontend})`, ...svgbg }}>
-          {/* <img src={frontend} style={svgImg} className='' alt='front end'  /> */}
+
           <h1 className="text-3xl text-center text-gray-200 mt-3">Front-end</h1>
           <ul className="self-center mx-auto text-gray-200 p-6" >
             {skillTypes.frontend.map((skill, i) => <li key={i} className='my-1 text-lg'> {skill}</li>)}
@@ -42,9 +34,6 @@ export default function Skills() {
         </div>
 
         <div className={skillClasses} style={{ backgroundImage: `url(${back})`, ...svgbg }} >
-
-          {/* <img src={back} style={svgImg} alt='back end ' /> */}
-
           <h1 className=" text-3xl text-center text-gray-200 mt-3">Back-end</h1>
           <ul className="justify-center  mx-auto text-gray-200 p-6">
             {skillTypes.backend.map((skill, i) => <li key={i} className='my-1 text-lg'>{skill}</li>)}
@@ -52,8 +41,6 @@ export default function Skills() {
         </div>
 
         <div className={skillClasses} style={{ backgroundImage: `url(${db})`, ...svgbg }} >
-          {/* <img src={db} style={svgImg} alt='database end ' /> */}
-
           <h1 className="text-3xl text-center text-gray-200 mt-3">Database</h1>
           <ul className="self-center w-3/4 mx-auto text-gray-200 p-6">
             {skillTypes.databases.map((skill, i) => <li key={i} className='my-1 text-lg'>{skill}</li>)}
@@ -65,8 +52,6 @@ export default function Skills() {
       <div className="flex-row lg:flex mt-5" data-sal="slide-down" data-sal-delay="350">
         <div className={skillClasses} style={{ backgroundImage: `url(${devop})`, ...svgbg }}>
 
-          {/* <img src={devop} style={svgImg} alt='devops ' /> */}
-
           <h1 className='text-3xl text-center text-gray-200 mt-3'>Devops</h1>
           <ul className="self-center mx-auto  text-gray-200 p-6" >
             {skillTypes.devops.map((skill, i) => <li key={i} className='my-1 text-lg'>{skill}</li>)}
@@ -74,9 +59,6 @@ export default function Skills() {
         </div>
 
         <div className={skillClasses} style={{ backgroundImage: `url(${extras})`, ...svgbg }}>
-
-          {/* <img src={extras} style={svgImg} alt='extras ' style={{ backgroundImage: `url(${frontend})`, ...svgbg }} /> */}
-
           <h1 className='text-3xl text-center text-gray-200 mt-3'>Extras</h1>
           <ul className="self-center mx-auto text-gray-200 p-6" >
             {skillTypes.extras.map((skill, i) => <li key={i} className='my-1 text-lg'>{skill}</li>)}
