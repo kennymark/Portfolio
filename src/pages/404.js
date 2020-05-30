7bd04a9fece9c1c5a353b5fadd15684f7df47b53
Kenny Mark <geniounico@outlook.com>
2020-05-30 18:14:28 +0100

    edited page 404 to look better

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,8 +5,8 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1 className='text-4xl text-center'>NOT FOUND</h1>
-    <p className='mt-4 text-center'>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1 className='text-6xl text-center capitalize dark:text-gray-500'>NOT FOUND</h1>
+    <p className='mt-4 text-center dark:text-gray-600'>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
 
