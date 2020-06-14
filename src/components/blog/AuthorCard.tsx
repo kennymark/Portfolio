48a259c6d59cb1de8a257e5ade788801f90a1617
Kenny Mark <geniounico@outlook.com>
2020-06-14 23:46:00 +0100

    actually replaxed it

--- src/components/blog/AuthorCard.tsx
+++ src/components/blog/AuthorCard.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 function AuthorCard({ author }) {
   return (
     <Flex alignItems='center'>
-      <Avatar name="Dan Abrahmov" size='sm' src="/static/d9e73527234ffec65fa70971c88df689/14b42/me.jpg" mr={3} />
+      <Avatar name="me" size='sm' src="/static/d9e73527234ffec65fa70971c88df689/14b42/me.jpg" mr={3} />
       <NLink as={Link} to='/profile' _hover={{ color: 'green.500' }} _focus={{ outline: 0 }} fontWeight='600' color='gray.500' >{author} </NLink>
     </Flex>
   )
