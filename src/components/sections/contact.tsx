d55032c17788d705a1e9d6dcc05b80cbaa13f57f
Kenny Mark <geniounico@outlook.com>
2020-06-15 03:10:51 +0100

    fixed ts on contact form

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -1,24 +1,23 @@
 
-//@ts-nocheck
-import { Box, Button, FormControl, FormLabel, Input, Stack, Textarea, useColorMode, useToast, Text, FormErrorMessage } from '@chakra-ui/core';
+
+import { Box, Button, FormControl, FormLabel, Input, Stack, Textarea, useColorMode, useToast, Text, } from '@chakra-ui/core';
 import React from 'react';
 import { useForm, ErrorMessage } from 'react-hook-form';
 import PageHeader from '../ui/page-header';
-import { flatMap } from 'lodash';
 
 function Contact() {
-  const { register, handleSubmit, errors, reset } = useForm();
+  const { register, handleSubmit, errors, reset, control } = useForm();
 
   const toast = useToast()
   const { colorMode } = useColorMode()
 
   const commonToast = {
-    position: 'top-right',
+    position: "top-right",
     variant: 'subtle',
     isClosable: true
-  }
+  } as any
 
-  const props = {
+  const inputProps = {
     size: 'lg',
     rounded: 'md',
     border: '1px',
@@ -29,7 +28,7 @@ function Contact() {
       border: '1px',
       borderColor: colorMode === 'light' ? 'gray.400' : 'green.900',
     }
-  }
+  } as any
 
   const sendMail = async (data) => {
 
@@ -77,25 +76,25 @@ function Contact() {
 
           <FormControl mb={5}>
             <FormLabel htmlFor="subject" color='gray.600'>Subject</FormLabel>
-            <Input type="text" name="subject" aria-describedby="subject-helper-text" {...props} ref={register(setValidation('Subject'))} />
+            <Input type="text" name="subject" aria-describedby="subject-helper-text" {...inputProps} ref={register(setValidation('Subject'))} />
             <ErrorMessage errors={errors} name='subject' as={<Text color='red.600' />} />
           </FormControl>
 
           <FormControl>
             <FormLabel htmlFor="name" color='gray.600'>Name</FormLabel>
-            <Input type="name" name="name" ref={register(setValidation('Name'))} aria-describedby="name-helper-text" {...props} />
+            <Input type="name" name="name" ref={register(setValidation('Name'))} aria-describedby="name-helper-text" {...inputProps} />
             <ErrorMessage errors={errors} name='name' as={<Text color='red.600' />} />
           </FormControl>
 
           <FormControl>
             <FormLabel htmlFor="email" color='gray.600'>Email address</FormLabel>
-            <Input type="email" name="email" ref={register(setValidation('Email'))} aria-describedby="email-helper-text" {...props} />
+            <Input type="email" name="email" ref={register(setValidation('Email'))} aria-describedby="email-helper-text" {...inputProps} />
             <ErrorMessage errors={errors} name='email' as={<Text color='red.600' />} />
           </FormControl>
 
           <FormControl>
             <FormLabel htmlFor="email" color='gray.600'>Message</FormLabel>
-            <Textarea type='textarea' name="message" ref={register(setValidation('Message', true))} aria-describedby="message-box" {...props} h={300} />
+            <Textarea type='textarea' name="message" ref={register(setValidation('Message', true))} aria-describedby="message-box" {...inputProps} h={300} />
             <ErrorMessage errors={errors} name='message' as={<Text color='red.600' />} />
           </FormControl>
 
