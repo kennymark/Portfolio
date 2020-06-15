ef21e00a4286dbe527aa1402f1c59b7f7b72df06
Kenny Mark <geniounico@outlook.com>
2020-06-15 11:26:30 +0100

    modified error 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,11 +5,11 @@ import { Heading, Text, Box } from "@chakra-ui/core"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
+    <SEO title="Error 404" />
 
     <Box mt={5}>
-      <Heading size='2xl' textAlign='center'>NOT FOUND</Heading>
-      <Text>You just hit a route that doesn&#39;t exist... the sadness.</Text>
+      <Heading size='2xl' textAlign='center'>Error 404: Not found</Heading>
+      <Text textAlign='center' color='gray.500'>This page does not seem to exist</Text>
     </Box>
 
   </Layout>
